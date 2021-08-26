453f14370ea76f633473588c53eeba8873a28d7d
EstefanyVergara <vteffa@gmail.com>
2021-08-26 17:35:47 -0400

    Post con data alojada en firebase funcionando :D

--- views/feedTemplate.js
+++ views/feedTemplate.js
@@ -3,24 +3,28 @@ import { firebaseFunctions } from "../lib/firebase.js";
 export const feed = () => {
   const feedPrincipal = document.createElement("div");
   const viewFeed = /*html*/ `
-    <div class ="containerPost" id="containerPost">
-    <div class ="formPost" id="postForm">
-        <div class ="profileData">
-        <img alt="" id="imgUser" class= "imgUser"/><br>
-        <div class ="nameUser" id="nameUser"></div>
-        </div>
-        </div>  
-        <input type= 'text' id= "commentPost" class="commentPost" placeholder= "¿Qué cocino hoy?" required> <br>
-        <form class="botones">
-        <button class="icon"><i class="fas fa-camera"></i></button>   
-        <button type='submit' class="publicButton" id="publicButton">PUBLICAR</button>       
-        </form>  
+  <div class ="containerPost" id="containerPost">
+  <div class ="dataPost" id="postForm">
+      <div class ="profileData">
+      <img alt="" id="imgUser" class= "imgUser"/><br>
+      <div class ="nameUser" id="nameUser"></div>
+      </div>
+      </div>
+      <form id ="feedForm" class="feedForm">  
+      <input type= 'text' id= "commentPost" class="commentPost" placeholder= "¿Qué cocino hoy?"  autofocus> <br>
+      <div class = "botones">
+      <button class="icon"><i class="fas fa-camera"></i></button>   
+      <button type='submit' class="publicButton" id="publicButton">PUBLICAR</button> 
+      </div>
+      </form>  
     </div>
     <button id ="closed"> cerrar Sesión jeje</button>
 
 `;
 
-  feedPrincipal.innerHTML = viewFeed;
+feedPrincipal.innerHTML = viewFeed;
+
+  //<--------SACAMOS PROPIEDADES DE GOOGGLE------------->
 
   const userProfile = feedPrincipal.querySelector("#nameUser");
   const userImg = feedPrincipal.querySelector("#imgUser");
@@ -28,16 +32,35 @@ export const feed = () => {
     if (user) {
       userProfile.innerHTML = user.displayName;
       userImg.src = user.photoURL;
-      console.log(user.email);
     } else {
     console.log("no existe user");
     }
   });
 
-  const cerrarSesion = feedPrincipal.querySelector("#closed");
-  cerrarSesion.addEventListener("click", () => {
-    firebaseFunctions.closeSesion();
-  });
+//<--------CAPTURO LO QUE EL USUARIO COMENTA------------->
+
+  const printPost = feedPrincipal.querySelector('#feedForm'); 
+printPost.addEventListener("submit", (e) =>{
+e.preventDefault();
+
+  const postContent = feedPrincipal.querySelector('#commentPost').value;
+  if(postContent.trim() == ''){
+    alert('Entonces, ¿qué cocinarás hoy?')
+  } else{
+    firebaseFunctions.savePublish(postContent);
+    printPost.reset();
+  }
+})
+
+
+
+
+
+
+// const cerrarSesion = feedPrincipal.querySelector("#closed");
+//   cerrarSesion.addEventListener("click", () => {
+//     firebaseFunctions.closeSesion();
+//   });
 
   return feedPrincipal;
 };
